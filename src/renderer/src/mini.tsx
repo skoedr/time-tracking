7220048e86e1024fa308f2e94f52e6d8e57c0c96
Robin <62442065+skoedr@users.noreply.github.com>
2026-05-29 15:30:31 +0200

    fix(mini): wrap mini widget in SettingsProvider (v1.13.1) (#123)
    Mini-Widget crashed on start with 'useSettings must be used within a SettingsProvider'.
    Since v1.12 (#106) I18nProvider depends on SettingsProvider, but mini.tsx never added it.

--- src/renderer/src/mini.tsx
+++ src/renderer/src/mini.tsx
@@ -3,12 +3,15 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import MiniApp from './MiniApp'
+import { SettingsProvider } from './contexts/SettingsContext'
 import { I18nProvider } from './contexts/I18nContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <I18nProvider>
-      <MiniApp />
-    </I18nProvider>
+    <SettingsProvider>
+      <I18nProvider>
+        <MiniApp />
+      </I18nProvider>
+    </SettingsProvider>
   </StrictMode>
 )
